e93faf090c526cf38b27f0b27134ab53577d0a35
bryan <nlni18@yahoo.com>
2022-02-27 17:38:46 -0600

    drawing basic sprite, getting joystick input, moving along y only

--- src/game.asm
+++ src/game.asm
@@ -11,9 +11,15 @@
     seg.u Variables
     org $80
 
-kyleXPos         byte         
-kyleYPos         byte         
+KyleXPos         byte         
+KyleYPos         byte         
+KyleSpritePtr    word         
+KyleColorPtr     word
+KyleOffset       byte           
 
+; define constants
+
+KYLE_HEIGHT = 9  
 
 ; start our ROM code at memory address $F000
 
@@ -24,6 +30,24 @@ Reset:
     CLEAN_START              
 
 
+; inititialize variables
+
+    lda #50
+    sta KyleXPos              
+    lda #60
+    sta KyleYPos               ; set Kyle x and y
+
+    lda #<KyleSprite
+    sta KyleSpritePtr         
+    lda #>KyleSprite
+    sta KyleSpritePtr+1        ; set low and high bytes of the sprite bitmap of Kyle sprite pointer
+
+    lda #<KyleColor
+    sta KyleColorPtr          
+    lda #>KyleColor
+    sta KyleColorPtr+1         ; set low and high bytes of the color bitmap of Kyle color pointer
+
+
 ; main loop
 
 StartFrame:
@@ -31,42 +55,125 @@ StartFrame:
 ; display VSYNC and VBLANK
 
     lda #2
-    sta VBLANK               ; turn on VBLANK
-    sta VSYNC                ; turn on VSYNC
-    REPEAT 3
-        sta WSYNC            ; display 3 recommended lines of VSYNC
-    REPEND
+    sta VBLANK                 ; turn on VBLANK
+    sta VSYNC                  ; turn on VSYNC
+    
+    sta WSYNC 
+    sta WSYNC 
+    sta WSYNC                  
+
     lda #0
-    sta VSYNC                ; turn off VSYNC
-    REPEAT 37
-        sta WSYNC            ; display the 37 recommended lines of VBLANK
-    REPEND
-    sta VBLANK               ; turn off VBLANK
+    sta VSYNC                  ; turn off VSYNC
+
+    ldy #37
+.VBlankLoop:
+    sta WSYNC 
+    dey
+    bne .VBlankLoop
+
+    sta VBLANK                 ; turn off VBLANK
 
 
-; display the 192 visible scanlines of our main game
+; display scanlines of game
 
 GameVisibleLine:
+    lda #$0
+    sta COLUBK                 ; set background to black
 
-    ldx #192                 ; X counts the number of remaining scanlines
+    ldx #96                    ; x counts the number of remaining scanlines
+
 .GameLineLoop:
-    sta WSYNC
-    dex                      ; X--
-    bne .GameLineLoop        ; repeat next main game scanline until finished
+    
+.InsideKyleSprite:          
+    txa                        
+    sec                        
+    sbc KyleYPos               
+    cmp KYLE_HEIGHT            
+    bcc .DrawSpriteP0          
+    lda #0                     ; save a, set carry flag, subtract Kyle position, compare with height, jump to draw if within bounds
 
+.DrawSpriteP0:
+    clc                      
+    adc KyleOffset        
+    tay                      
+    lda (KyleSpritePtr),Y     
+    sta WSYNC                
+    sta GRP0                 
+    lda (KyleColorPtr),Y      
+    sta COLUP0                 ; get offset into y, jump to bitmap location for sprite plus y, load player register, jump to color location plus y, load color 
+
+    sta WSYNC                
+
+    dex                      
+    bne .GameLineLoop          ; decrement x, if not zero jump back up to GameLineLoop
+
+    lda #0
+    sta KyleOffset             ; reset Kyle offset
+
+    sta WSYNC                
 
 ; dsplay overscan
-    lda #2
-    sta VBLANK               ; turn on VBLANK again
-    REPEAT 30
-        sta WSYNC            ; display 30 recommended lines of VBlank Overscan
-    REPEND
-    lda #0
-    sta VBLANK               ; turn off VBLANK
 
-; loop back to start a brand new frame
-   
-    jmp StartFrame           ; continue to display the next frame
+    lda #2
+    sta VBLANK                 ; turn on VBLANK again
+
+    ldy #37
+.OverScanLoop:
+    sta WSYNC 
+    dey
+    bne .OverScanLoop
+
+    lda #0
+    sta VBLANK                 ; turn off VBLANK
+
+
+; check joystick
+
+CheckP0Up:
+    lda #%00010000             
+    bit SWCHA
+    bne CheckP0Down          
+    inc KyleYPos
+    lda #0
+    sta KyleOffset             ; check if joystick up is pressed, if so increment y position, else fall through to next check
+
+CheckP0Down:
+    lda #%00100000          
+    bit SWCHA
+    bne EndInputCheck          
+    dec KyleYPos
+    lda #0
+    sta KyleOffset             ; check if joystick down is pressed, if so decrement y position, else fall through to end checks
+
+EndInputCheck:
+
+; end of main loop, jump back to start 
+
+    jmp StartFrame           
+
+; sprite and color bitmaps
+
+KyleSprite:
+    .byte #%00000000         ;
+    .byte #%00001111         ; #####  
+    .byte #%00001111         ; #####
+    .byte #%00001111         ; #####
+    .byte #%00001111         ; #####
+    .byte #%00001111         ; #####
+    .byte #%00001111         ; ##### 
+    .byte #%00001111         ; #####   
+    .byte #%00001111         ; #####   
+
+KyleColor:
+    .byte #$00
+    .byte #$0E
+    .byte #$0E
+    .byte #$0E
+    .byte #$0E
+    .byte #$0E
+    .byte #$0E
+    .byte #$0E
+    .byte #$0E
 
 ; complete ROM size with exactly 4KB
    
